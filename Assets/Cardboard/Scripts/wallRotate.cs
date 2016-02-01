862a5f45fc1e4ab1a7ff8710b664263edc42e5f1
KennaW <KennaWarsinske@gmail.com>
2016-02-01 14:46:15 -0800

    it moves in a circle now

---
+++ Assets/Cardboard/Scripts/wallRotate.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class wallRotate : MonoBehaviour {
+	public float turnSpeed = 10f;
+
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+	}
+}
